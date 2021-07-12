7d9a91a43e9728fd83815eb6ada4136301d54fcf
haoScoville <hao@sc0ville.com>
2021-07-12 10:33:32 +0700

    Add log to detect error

--- templates/code/logsToEs.js
+++ templates/code/logsToEs.js
@@ -189,6 +189,7 @@ function post(body, callback) {
             responseBody += chunk;
         });
         response.on('end', function() {
+            console.log("RESPONSE BODY", responseBody)
             var info = JSON.parse(responseBody);
             var failedItems;
             var success;
